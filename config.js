2ad6a0510e0e6f93672fa114296124848431dd59
Tyler Skidmore <tyler.s9@proton.me>
2024-07-03 19:30:13 -0400

    Fixed db and config files

--- config.js
+++ config.js
@@ -1,13 +1,35 @@
-/** Common config for bookstore. */
+// /** Common config for bookstore. */
 
 
-let DB_URI = `postgresql://`;
+// let DB_URI = `postgresql://`;
 
-if (process.env.NODE_ENV === "test") {
-  DB_URI = `${DB_URI}/books-test`;
+// if (process.env.NODE_ENV === "test") {
+//   DB_URI = `${DB_URI}/books-test`;
+// } else {
+//   DB_URI = process.env.DATABASE_URL || `${DB_URI}/books`;
+// }
+
+
+// module.exports = { DB_URI };
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const NODE_ENV = process.env.NODE_ENV || 'development';
+
+let DB_URI, SECRET_KEY, BCRYPT_WORK_FACTOR;
+
+if (NODE_ENV === 'test') {
+  DB_URI = "postgresql:///books_test";
 } else {
-  DB_URI = process.env.DATABASE_URL || `${DB_URI}/books`;
+  DB_URI = process.env.DATABASE_URL || "postgresql:///books";
 }
 
+SECRET_KEY = process.env.SECRET_KEY;
+BCRYPT_WORK_FACTOR = 12;
 
-module.exports = { DB_URI };
+module.exports = {
+  DB_URI,
+  SECRET_KEY,
+  BCRYPT_WORK_FACTOR
+};
--- db.js
+++ db.js
@@ -1,14 +1,33 @@
-/** Database config for database. */
+// /** Database config for database. */
 
 
-const { Client } = require("pg");
-const {DB_URI} = require("./config");
+// const { Client } = require("pg");
+// const {DB_URI} = require("./config");
 
-let db = new Client({
-  connectionString: DB_URI
+// let db = new Client({
+//   connectionString: DB_URI
+// });
+
+// db.connect();
+
+
+// module.exports = db;
+const { Client } = require('pg');
+const { DB_URI } = require('./config');
+
+const client = new Client({
+  connectionString: DB_URI,
 });
 
-db.connect();
+async function connectToDatabase() {
+  try {
+    await client.connect();
+    console.log('Connected to PostgreSQL');
+  } catch (error) {
+    console.error('Error connecting to PostgreSQL:', error);
+  }
+}
 
+connectToDatabase();
 
-module.exports = db;
+module.exports = client;
